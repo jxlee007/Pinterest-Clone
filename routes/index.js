a5fe90c166da42976a174747e370ef1296ad1fec
jagmohan <jimmyfalco007@gmail.com>
2023-12-18 20:44:41 +0530

    setting passport

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require("./users")
+const postModel = require("./posts")
+const localStrategy = require("passport-local");
+const passport = require('passport');
+passport.use(new localStrategy(userModel.authenticate())); 
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next){
+   res.send('index',{ title : "express"});
 });
+
+// register route using passport in local strategy
+router.post('/register', function(req, res){
+   const userdata = new userModel({
+   // use to take data from input field defines with names
+      username : req.body.username ,
+      email :req.body.email,
+      fullname :req.body.fullname,
+   });
+   // otherwise using object destructuring
+   // const { username, email, fullname } = req.body;
+   // const userdata = new userModel({ username, email, fullname});
+
+
+   // if register successfully redirect to profile route
+   userModel.register(userdata, req.body.password)
+   .then(function (registereduser){
+      passport.authenticate("local")(req, res, function(){
+         res.redirect('/profile');
+      })
+   })
+});
+
+router.post('/login', passport.authenticate('local', {
+      successRedirect: '/profile',
+      failureRedirect:"/"
+   }), function(req, res){
+});
+
 
 module.exports = router;
